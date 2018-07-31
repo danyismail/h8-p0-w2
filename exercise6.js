3cb2a4eadc1747a6ba66e05660bbcd5fac98cb2c
SIERA <siera@SIERAs-Air.localdomain>
2018-08-01 02:47:22 +0700

    correction name of file and add task2

---
+++ exercise6.js
@@ -0,0 +1,7 @@
+for(i=2; i<=20; i+=2){
+    console.log(i + ' ' + ' - ' + ' ' + 'I love coding')
+}
+console.log('')
+for(i=20; i>=2; i-=2){
+   console.log(i + ' ' + '-' + 'I' + ' ' + 'Will' + ' ' + 'become' + ' ' + 'stack' + ' ' + 'developer')
+}
