9321a9fa9fc73364fdd4461fd03987333c91805a
SIERA <siera@SIERAs-Air.localdomain>
2018-08-01 18:48:52 +0700

    combine loop n condition2

---
+++ exercise-6-combining-loop-con2.js
@@ -0,0 +1,23 @@
+for(j=1; j<=100; j+=2){
+    if(j%3 == 0){
+        console.log(j + ' ' + 'KELIPATAN 3')
+    }else{
+        console.log(j)
+    }
+}
+console.log('')
+for(k=1; k<=100; k+=5){
+    if(k%6 == 0){
+        console.log(k + ' ' + 'KELIPATAN 6')
+    }else{
+        console.log(k)
+    }
+}
+console.log('')
+for(n=1; n<=100; n+=9){
+    if(n%10 == 0){
+        console.log(n + ' ' + 'KELIPATAN 10')
+    }else{
+        console.log(n)
+    }
+}
