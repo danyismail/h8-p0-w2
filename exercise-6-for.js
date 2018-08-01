7f88642a955291982e4b1ec9f991af8620c28cdc
SIERA <siera@SIERAs-Air.localdomain>
2018-08-01 17:34:09 +0700

    exercise7 for

---
+++ exercise-6-for.js
@@ -0,0 +1,9 @@
+for(c=1; c<=20; c++){
+    console.log(c + ' ' + ' - ' + ' ' + 'I love coding')
+}
+
+console.log('')
+for(d=20; d>=1; d--){
+    console.log(d + ' ' + '-' + 'I' + ' ' + 'Will' + ' ' + 'become' + ' ' + 'stack' + ' ' + 'developer')
+}
+
