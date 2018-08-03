2850c98ed13b9723f326c82d1d012f52da261918
SIERA <siera@SIERAs-Air.localdomain>
2018-08-03 23:38:42 +0700

    merge content

--- exercise-7-1.js
+++ exercise-7-1.js
@@ -1,4 +1,29 @@
+//1. Menyusun Barisan Bintang
 var rows1   = 5
 for(i=1; i<=rows1; i++){
     console.log('*')
 }
+console.log('')
+
+//2. Menyusun Barisan Bintang Dengan Nested Looping
+var rows2   = 5
+var hash    = ''
+for (i=1; i<=rows2; i++) {
+    for (j=1; j<=rows2; j++){
+        hash += '*'
+    }
+    hash += '\n'    
+}
+console.log(hash)
+
+//3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+var rows2   = 5
+var hash    = ''
+for (i=1; i<=rows2; i++) {
+    for (j=1; j<=i; j++){
+        hash += '*' 
+        
+    }
+hash += '\n'        
+}
+console.log(hash)
