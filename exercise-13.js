c739bcddad80794dbed9670a8045231d437e340b
SIERA <siera@SIERAs-Air.localdomain>
2018-08-05 14:29:10 +0700

    tambah console.log

--- exercise-13.js
+++ exercise-13.js
@@ -25,7 +25,12 @@ if (hasilX == hasilY){
     return false
   }
 }
-console.log(xo('oooxx'))
+ console.log(xo('xoxoxo')); // true
+ console.log(xo('oxooxo')); // false
+ console.log(xo('oxo')); // false
+ console.log(xo('xxxooo')); // true
+ console.log(xo('xoxooxxo')); // true
+
 
 
 
